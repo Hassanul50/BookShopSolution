149f12c998cc207e89ea50b6d727295101659e92
Hassanul50 <hasanakib27@gmail.com>
2023-05-16 01:38:50 +0600

    comit to push category

---
+++ BookShopApp/Controllers/CategoryController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BookShopApp.Controllers
+{
+    public class CategoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ BookShopApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using BookShopApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookShopApp.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) 
+        {
+            
+        }
+        public DbSet<Category> Categories { get; set; }     
+    }
+}
