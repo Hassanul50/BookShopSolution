6c7f43f7a067e6da0ea587e61b917e709dd31f2b
hasan50 <hasanakib27@gmail.com>
2023-11-21 00:09:40 +0600

    commit to update

--- BookShopApp/Areas/Admin/Controllers/CategoryController.cs
+++ BookShopApp/Areas/Admin/Controllers/CategoryController.cs
@@ -4,19 +4,20 @@ using BookShopApp.Models;
 using BookShopApp.Repository.IRepository;
 using Microsoft.AspNetCore.Mvc;
 
-namespace BookShop.DataAccess.Controllers
+namespace BookShopApp.Areas.Admin.Controllers
 {
+    [Area("Admin")]
     public class CategoryController : Controller
     {
         private readonly ICategoryRepository categoryRepo;
         private readonly IUnitOfWork _unitOfWork;
         public CategoryController(IUnitOfWork unitOfWork)
         {
-            _unitOfWork = unitOfWork;      
+            _unitOfWork = unitOfWork;
         }
         public IActionResult Index()
         {
-            List<Category> BookList = _unitOfWork.Category.GetAll().ToList(); 
+            List<Category> BookList = _unitOfWork.Category.GetAll().ToList();
 
             return View(BookList);
         }
@@ -25,7 +26,8 @@ namespace BookShop.DataAccess.Controllers
             return View();
         }
         [HttpPost]
-        public IActionResult Create(Category obj) {
+        public IActionResult Create(Category obj)
+        {
             if (obj.Name == obj.DisplayOrder.ToString())
             {
                 ModelState.AddModelError("Name", "Display order should  be diffferent from Category name Name");
@@ -42,17 +44,17 @@ namespace BookShop.DataAccess.Controllers
             //    return View("please give some data");
             //}
             return View();
-            
+
         }
 
         public IActionResult Edit(int? id)
         {
-            if(id == null || id == 0)
+            if (id == null || id == 0)
             {
                 return NotFound();
             }
             //Category categoryFromDb = _db.Categories.Find(id);//only for primary key 
-            Category? categoryFromDb = _unitOfWork.Category.GetById(u=>u.Id==id);
+            Category? categoryFromDb = _unitOfWork.Category.GetById(u => u.Id == id);
             //Category? categoryFromDb2 = _db.Categories.Where(u=>u.Id==id).FirstOrDefault();
             if (categoryFromDb == null)
             {
@@ -63,7 +65,7 @@ namespace BookShop.DataAccess.Controllers
         [HttpPost]
         public IActionResult Edit(Category obj)
         {
-          
+
             if (ModelState.IsValid)
             {
                 _unitOfWork.Category.update(obj);
@@ -93,7 +95,7 @@ namespace BookShop.DataAccess.Controllers
             }
             return View(categoryFromDb);
         }
-        [HttpPost,ActionName("Delete")]
+        [HttpPost, ActionName("Delete")]
         public IActionResult DeletePost(int? id)
         {
             Category? categoryForDelete = _unitOfWork.Category.GetById(u => u.Id == id); ;
@@ -102,17 +104,17 @@ namespace BookShop.DataAccess.Controllers
                 return NotFound();
             }
 
-            
-                _unitOfWork.Category.Delete(categoryForDelete);
-                _unitOfWork.Save();
-                TempData["success"] = "Category Deleted succesfully";
+
+            _unitOfWork.Category.Delete(categoryForDelete);
+            _unitOfWork.Save();
+            TempData["success"] = "Category Deleted succesfully";
             return RedirectToAction("Index", "Category");
-            
+
             //else
             //{
             //    return View("please give some data");
             //}
-            
+
 
         }
 
